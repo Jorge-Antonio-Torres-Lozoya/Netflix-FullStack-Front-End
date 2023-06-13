1d9bdb99e56c0a41a45f9de3178dca2ef1cf2bf3
Jorge-Antonio-Torres-Lozoya <jtorres1@ucol.mx>
2023-06-13 16:28:15 -0600

    fix uri

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import MovieSubList from './components/MovieSubList';
 function App() {
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: "http://localhost:3000/",
+    uri: "https://outstanding-moth-glasses.cyclic.app",
   });
 
   return (
